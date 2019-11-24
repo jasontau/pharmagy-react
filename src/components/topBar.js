854c9aa39cc85199b600ce752e0398d832a5da8e
Jason Au <jasontau@gmail.com>
2019-11-24 14:45:46 -0800

    added profile, dateButton, patientCard to topBar

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -1,19 +1,76 @@
 import React from 'react';
 import SearchBar from './searchBar';
+import Profile from './profile';
+import DateButton from './dateButton';
+import PatientCard from './patientCard'
 import styled from 'styled-components';
 
 const BackgroundTop = styled.div`
   width: 375px;
   height: 246px;
   background-image: linear-gradient(131deg, #2d79e6 -18%, #053476 113%);
+`;
+
+const Container = styled.div`
+  width: inherit;
   position: relative;
+  height: 169px;
+`;
+
+const SearchContainer = styled.div`
+  margin-top: 47px;
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  display: flex;
+  width: inherit;
+`;
+
+const PatientsSummary = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 35px;
+`;
+
+const MyPatients = styled.div`
+  font-size: 18px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+`;
+
+const TotalPatients = styled.div`
+  font-size: 12px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.42;
+  letter-spacing: 0.3px;
+  color: #c2ddff;
+  text-align: left;
 `;
 
 function TopBar() {
   return (
+    <>
     <BackgroundTop>
-      <SearchBar />
+      <Container>
+        <SearchContainer>
+          <SearchBar />
+          <Profile />
+        </SearchContainer>
+        <DateButton text='Today' />
+        <PatientsSummary>
+          <MyPatients>My Patients</MyPatients>
+          <TotalPatients>12 total</TotalPatients>
+        </PatientsSummary>
+      </Container>
+      
     </BackgroundTop>
+    <PatientCard />
+    </>
   );
 }
 
