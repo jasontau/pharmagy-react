603bda98413a0903871c0bde3c569acb6da107d3
Jason Au <jasontau@gmail.com>
2019-11-24 14:45:22 -0800

    added text component

---
+++ src/components/text.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+import theme from '../theme';
+
+const AppText = styled.div`
+  font-size: ${props => props.size || '12'}px;
+  font-weight: ${props => theme[props.weight] || 'normal'};
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.42;
+  letter-spacing: 0.3px;
+  color: ${props => theme[props.color] || theme.secondary};
+`;
+
+function Text({ text, ...rest }) {
+  return <AppText {...rest}>{text}</AppText>;
+}
+
+export default Text;
