e43284cff3ef1e6924b197d3ff5e4f2390409a2f
Jason Au <jasontau@gmail.com>
2019-11-23 20:24:53 -0800

    added searchBar

---
+++ src/components/searchBar.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import styled from 'styled-components';
+import { makeStyles } from '@material-ui/core/styles';
+
+const Container = styled.div`
+  width: 240px;
+  height: 45px;
+  margin-top: 47px;
+  margin-left: 35px;
+  border-radius: 31px;
+  background-color: rgb(61, 139, 255, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+`;
+
+const Input = styled.input`
+  flex-grow: 1;
+  margin-left: 23px;
+  border: none;
+  background: transparent;
+  outline: none;
+  caret-color: #c2ddff;
+  font-size: 12px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.42;
+  letter-spacing: 0.3px;
+  color: #c2ddff;
+  &::-webkit-input-placeholder {
+    color: #c2ddff;
+  }
+`;
+
+const useStyles = makeStyles({
+  icon: {
+    fontSize: '15px',
+    marginRight: '20px'
+  }
+});
+
+function SearchBar() {
+  const classes = useStyles();
+  return (
+    <Container>
+      <Input placeholder='Search' />
+      <SearchIcon className={classes.icon}></SearchIcon>
+    </Container>
+  );
+}
+
+export default SearchBar;
--- src/components/topBar.js
+++ src/components/topBar.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import SearchBar from './searchBar';
 import styled from 'styled-components';
 
 const BackgroundTop = styled.div`
   width: 375px;
   height: 246px;
   background-image: linear-gradient(131deg, #2d79e6 -18%, #053476 113%);
+  position: relative;
 `;
 
 function TopBar() {
-  return <BackgroundTop />;
+  return (
+    <BackgroundTop>
+      <SearchBar />
+    </BackgroundTop>
+  );
 }
 
 export default TopBar;
