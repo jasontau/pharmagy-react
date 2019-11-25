b91eb76af40306adf4d97005b94dbceec05697a6
Jason Au <jasontau@gmail.com>
2019-11-24 21:47:23 -0800

    added schedule to app

--- src/components/bottomBar.js
+++ src/components/bottomBar.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import Schedule from '../components/schedule';
 
 const BackgroundBottom = styled.div`
   width: 375px;
   height: 498px;
-  background-image: linear-gradient(to bottom, #f8fbff, #e2e9f5);
+  background: linear-gradient(to bottom, #f8fbff, #e2e9f5);
+  margin-top: -52px;
 `;
 
 function TopBar() {
-  return <BackgroundBottom />;
+  return (
+      <BackgroundBottom>
+        <Schedule/>
+      </BackgroundBottom>
+  );
 }
 
 export default TopBar;
