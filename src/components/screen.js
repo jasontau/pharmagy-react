bf7ee091adb3ad0dd293514a6306d5acf1787842
Jason Au <jasontau@gmail.com>
2019-11-24 21:46:51 -0800

    clipped swiper elements

--- src/components/screen.js
+++ src/components/screen.js
@@ -4,9 +4,9 @@ import styled from 'styled-components';
 const Container = styled.div`
   min-height: 809px;
   min-width: 375px;
-  border: 1px solid black;
   background: #fff;
   position: relative;
+  clip-path: inset(0px 0px)
 `;
 
 function Screen({ children }) {
